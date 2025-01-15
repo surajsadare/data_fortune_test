a4d255ff56614c2151512deb6c3deb57da4ff554
surajsadare <surajsadare9@gmail.com>
2025-01-16 00:20:11 +0530

    basic structure

---
+++ registration_form/src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
+})
+export class SharedModule {}
